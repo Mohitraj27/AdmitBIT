b5935cb2c858c1881e520c446eeddbc33df5bd6b
Mohit Raj <raj716980@gmail.com>
2022-12-03 22:47:35 +0530

    Payment Data Saved to Firebase as a Draft

--- app/src/main/java/com/example/se_firebase/Fee_payment.java
+++ app/src/main/java/com/example/se_firebase/Fee_payment.java
@@ -1,5 +1,6 @@
 package com.example.se_firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
@@ -8,17 +9,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.razorpay.Checkout;
 import com.razorpay.PaymentResultListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
+
 public class Fee_payment extends AppCompatActivity implements PaymentResultListener {
 
   private   TextInputEditText Current_sem,Payee_name,Amount,Contact_no;
-  private   Button PayNow;
+  private   Button PayNow,Save_as_draft;
+
+
+  //Saving Payment Data as a Draft
+
+    private FirebaseDatabase db=FirebaseDatabase.getInstance();
+    private DatabaseReference root=db.getReference().child("Fee Payment Data");
+
+
+
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -30,6 +47,44 @@ public class Fee_payment extends AppCompatActivity implements PaymentResultListe
         Current_sem = findViewById(R.id.edit_text_semester_payment);
         Amount = findViewById(R.id.edit_payment_amount);
      Contact_no=findViewById(R.id.Contact_no);
+
+     //Code for saving fee details as a draft to database
+     Save_as_draft=findViewById(R.id.save_fee_payment);
+     Save_as_draft.setOnClickListener(new View.OnClickListener() {
+         @Override
+         public void onClick(View view) {
+
+
+             String name=Payee_name.getText().toString();
+             String Currentsem=Current_sem.getText().toString();
+             String amountfee=Amount.getText().toString();
+             String contactno=Contact_no.getText().toString();
+
+
+
+             HashMap<String, String> userMap=new HashMap<>();
+             userMap.put("Full Name of Student ", name);
+             userMap.put("Current Semester ", Currentsem);
+             userMap.put("Amount to be paid ", amountfee);
+             userMap.put("Comtact Number", contactno);
+
+
+             root.push().setValue(userMap)
+                     .addOnCompleteListener(new OnCompleteListener<Void>() {
+                         @Override
+                         public void onComplete(@NonNull Task<Void> task) {
+                             Toast.makeText(Fee_payment.this,"Your Fee Details is saved with us as a Draft!!",Toast.LENGTH_SHORT).show();
+                         }
+                     })
+                     .addOnFailureListener(new OnFailureListener() {
+                         @Override
+                         public void onFailure(@NonNull Exception e) {
+                             Toast.makeText(Fee_payment.this,"Error -"+e.getMessage(),Toast.LENGTH_SHORT).show();
+                         }
+                     });
+
+         }
+     });
 
         PayNow = findViewById(R.id.pay_FEE_now);
         PayNow.setOnClickListener(new View.OnClickListener() {
